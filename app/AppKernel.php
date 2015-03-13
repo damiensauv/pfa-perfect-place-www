7661039580cbe048eef689e362924e4d744062c9
Damien sauvalle <damien.sauvalle@epitech.eu>
2015-03-13 15:20:22 +0100

    jquery

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new PerfectPlace\CoreBundle\PerfectPlaceCoreBundle(),
 	    new Bmatzner\FoundationBundle\BmatznerFoundationBundle(),
+	    new Bmatzner\JQueryBundle\BmatznerJQueryBundle(),
             new PerfectPlace\UserBundle\PerfectPlaceUserBundle(),
         );
 
