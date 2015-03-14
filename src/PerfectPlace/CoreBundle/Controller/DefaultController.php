949503170a54d37e8b86640b9289e4be77914cd8
versme_a <versmee.alexandre@gmail.com>
2015-03-15 00:58:22 +0100

    route done

--- src/PerfectPlace/CoreBundle/Controller/DefaultController.php
+++ src/PerfectPlace/CoreBundle/Controller/DefaultController.php
@@ -10,25 +10,8 @@ class DefaultController extends Controller
 {
     public function indexAction(Request $request)
     {
-
-/*
- *
- * Type Pro
- *
-        $form = $this->createFormBuilder()
-            ->add('commerce', 'integer')
-            ->add('transport', 'integer')
-            ->add('nocturne', 'integer')
-            ->add('sante', 'integer')
-             ->add('rayon', 'integer')
-            ->add('save', 'submit')
-            ->getForm();
-*/
-
-        /*
-         * Type particulier
-         */
-        $typefield = array('famille' => 'Famille', 'etudiant' => 'étudiant', 'age' => 'personne age', 'handicape' => 'handicape');
+        $typefield = array('famille' => 'Famille', 'etudiant' => 'étudiant', 'age' => 'personne age',
+            'handicape' => 'handicape');
         $form = $this->createFormBuilder()
             ->add('mobilite', 'integer')
             ->add('environement', 'integer')
@@ -36,15 +19,44 @@ class DefaultController extends Controller
             ->add('nocturne', 'integer')
             ->add('commerce', 'integer')
             ->add('sante', 'integer')
-            ->add('type','choice', array('choices' => $typefield))
+            ->add('type', 'choice', array('choices' => $typefield))
             ->add('rayon', 'integer')
             ->add('Rechercher', 'submit')
             ->getForm();
 
         $form->handleRequest($request);
 
-        if ($form->isValid())
-        {
+        if ($form->isValid()) {
+            $data = $form->getData();
+            /*
+                * A JSON request example :
+                {
+                "origin": [12, 34],
+                "radius": 15,
+                "criterias":
+                {
+                    "transport": 50,
+                    "nature": 25
+                 }
+                }*/
+        }
+        return $this->render('PerfectPlaceCoreBundle:Default:index.html.twig', array('form' => $form->createView()));
+    }
+
+    public function professionnelAction(Request $request)
+    {
+        $form = $this->createFormBuilder()
+            ->add('commerce', 'integer')
+            ->add('transport', 'integer')
+            ->add('nocturne', 'integer')
+            ->add('sante', 'integer')
+            ->add('rayon', 'integer')
+            ->add('Rechercher', 'submit')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
             $data = $form->getData();
 
             /*
@@ -57,24 +69,12 @@ class DefaultController extends Controller
                     "transport": 50,
                     "nature": 25
                  }
-                }
-            */
-
-
+                }*/
         }
-
-        return $this->render('PerfectPlaceCoreBundle:Default:index.html.twig', array('form' => $form->createView()));
+        return $this->render('PerfectPlaceCoreBundle:Default:professionnel.html.twig',
+            array('form' => $form->createView()));
     }
 
-    public function profesionnelAction(Request $request)
-    {
-        return $this->render('PerfectPlaceCoreBundle:Default:index.html.twig', array());
-    }
-
-    public function particulierAction(Request $request)
-    {
-        return $this->render('PerfectPlaceCoreBundle:Default:index.html.twig', array());
-    }
 }
 
 
