2b0ebd6535d4aa5485e96ea1ce7d07db401315f0
Damien sauvalle <damien.sauvalle@epitech.eu>
2015-03-15 13:58:54 +0100

    yolo

--- src/PerfectPlace/CoreBundle/Controller/DefaultController.php
+++ src/PerfectPlace/CoreBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace PerfectPlace\CoreBundle\Controller;
 
 use PerfectPlace\CoreBundle\Entity\Particulier;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -18,31 +19,90 @@ class DefaultController extends Controller
             ->add('commerce', 'integer')
             ->add('sante', 'integer')
             ->add('rayon', 'integer')
+            ->add('lat', 'integer', array('attr' => array('style' => 'display: none')))
             ->add('Rechercher', 'submit')
             ->getForm();
 
         $form->handleRequest($request);
 
-        if ($form->isValid()) {
+        if ($form->isValid())
+        {
             $data = $form->getData();
 
+            print_r($data);
 
-            var_dump($data);
 
+            $donne = array();
+            $donne['origin'] = array(50.6355677, 3.0620463);
+            $donne['radius'] = $data['rayon'];
+            $donne['criterias'] = array(
+                'subway' => $data['mobilite'],
+                'bicycle_rental' => $data['mobilite'],
+                'bus'  => $data['mobilite'],
+                'parking' => $data['mobilite'],
+                'tramway' => $data['mobilite'],
+                'recycling' => $data['environement'],
+                'fountain' => $data['environement'],
+                'garden' => $data['environement'],
+                'park' => $data['environement'],
+                'arts' => $data['culture'],
+                'cinema' => $data['culture'],
+                'theatre' => $data['culture'],
+                'stadium' => $data['culture'],
+                'museum' => $data['culture'],
+                'zoo'  => $data['culture'],
+                'bar'  => $data['nocturne'],
+                'cafe' => $data['nocturne'],
+                'fast_food' => $data['nocturne'],
+                'pub'  => $data['nocturne'],
+                'restaurant' => $data['nocturne'],
+                'casino' => $data['nocturne'],
+                'night_club' => $data['nocturne'],
+                'bank' => $data['commerce'],
+                'bakery' => $data['commerce'],
+                'store'  => $data['commerce'],
+                'mall'  => $data['commerce'],
+                'supermarket' => $data['commerce'],
+                'clinic' => $data['sante'],
+                'doctors' => $data['sante'],
+                'hospital' => $data['sante'],
+                'pharmacy' => $data['sante'],
+                'chemist' => $data['sante']
+            );
+
+            $json = json_encode($donne);
 
             /*
-                * A JSON request example :
-                {
-                "origin": [12, 34],
-                "radius": 15,
-                "criterias":
-                {
-                    "transport": 50,
-                    "nature": 25
-                 }
-                }*/
+            $lien = "http://localhost/pfa-perfect-place-api/web/app_dev.php/api/request";
+            $curl = curl_init();
+            $header[] = "Content-Type: application/json";
+            curl_setopt($curl, CURLOPT_URL, $lien);
+            curl_setopt($curl, CURLOPT_COOKIESESSION, true);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $json);
+
+            $return = curl_exec($curl);
+            curl_close($curl);
+
+            $decode = json_decode($return , true);
+
+            $i = 0;
+            while ($decode['areas'][$i]['weight'] < 0.5)
+            {
+                $i = $i + 1;
+            }
+            $coord = array("lat" => $decode['areas'][$i]['origin'][0], "long" => $decode['areas'][$i]['origin'][1]);
+            $radius = $decode['areas'][$i]['radius'];
+*/
+
+  //          return $this->render('PerfectPlaceCoreBundle:Default:index.html.twig', array('form' => $form->createView(), 'coord' => $coord, 'radius' => $radius, 'ok' => 1));
         }
-        return $this->render('PerfectPlaceCoreBundle:Default:index.html.twig', array('form' => $form->createView()));
+
+        $coord = array("lat" => 50.6355677 , "long" =>  3.06204630000002);
+        $radius = 0;
+
+        return $this->render('PerfectPlaceCoreBundle:Default:index.html.twig', array('form' => $form->createView(), 'coord' => $coord, 'radius' => 0, 'ok' => 0));
     }
 
     public function professionnelAction(Request $request)
